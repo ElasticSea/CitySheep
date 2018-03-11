597fda952e616afec966a9b2cfee799f5ac95658
ElasticSea <info@elasticsea.com>
2018-03-11 15:46:55 +0100

    Moving sheep over map

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class InputManager : MonoBehaviour
+    {
+        [SerializeField] private Player player;
+
+        private void Update()
+        {
+            var axisInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+
+            if (player.IsMoving)
+            {
+                return;
+            }
+
+            if (axisInput.x < 0)
+            {
+                player.Move(-1, 0);
+                return;
+            }
+
+            if (axisInput.x > 0)
+            {
+                player.Move(1, 0);
+                return;
+            }
+
+            if (axisInput.y < 0)
+            {
+                player.Move(0, -1);
+                return;
+            }
+
+            if (axisInput.y > 0)
+            {
+                player.Move(0, 1);
+                return;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,23 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Tweener breathing;
+    private Tweener moving;
+    public bool IsMoving => moving != null && moving.IsPlaying();
+
+    private void Awake()
+    {
+        breathing = transform.DOScaleY(.9f, 1).SetEase(Ease.InOutQuad).SetLoops(-1, LoopType.Yoyo);
+        Move(1, 0);
+    }
+
+    public void Move(int x, int y)
+    {
+        var dir = new Vector3(x, 0, y);
+        transform.LookAt(transform.position + dir);
+        moving = transform.DOMove(transform.position + dir, .3f).SetEase(Ease.OutCubic);
+        transform.DOMoveY(.3f, .15f).SetEase(Ease.OutCubic).SetLoops(2, LoopType.Yoyo);
+    }
+}
