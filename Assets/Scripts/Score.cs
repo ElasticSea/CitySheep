d863188c365cfb326903d8e1fdc53988c43d434d
ElasticSea <info@elasticsea.com>
2018-03-12 14:56:37 +0100

    Adding ui score.

---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,40 @@
+﻿using DG.Tweening;
+using TMPro;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class Score : MonoBehaviour
+    {
+        [SerializeField] private GameObject player;
+        [SerializeField] private TextMeshProUGUI score;
+        private Tween tween;
+
+        public int ScoreValue
+        {
+            get { return int.Parse(score.text); }
+            set { score.text = value.ToString(); }
+        }
+
+        private void Awake()
+        {
+            ScoreValue = 0;
+        }
+
+        private void Update()
+        {
+            var currentScore = Mathf.CeilToInt(player.transform.position.z);
+            if (currentScore > ScoreValue)
+            {
+                ScoreValue = currentScore;
+                if(tween != null && tween.IsPlaying()) tween.Complete();
+
+                tween = DOTween.Sequence()
+                    .Insert(0, score.transform.DOLocalMoveY(score.transform.localPosition.y + 10, .07f))
+//                    .Insert(0, score.transform.DOScale(Vector3.one * 1.1f, .07f))
+                    .SetEase(Ease.OutCubic)
+                    .SetLoops(2, LoopType.Yoyo);
+            }
+        }
+    }
+}
