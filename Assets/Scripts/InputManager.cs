ae3f99210c4c30b21fa97e55d92350ed3728aa27
ElasticSea <info@elasticsea.com>
2018-03-12 18:55:38 +0100

    Adding gestures to the game.

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -1,11 +1,37 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace Assets.Scripts
 {
     public class InputManager : MonoBehaviour
     {
         [SerializeField] private Player player;
+        [SerializeField] private MiniGestureRecognizer gestureRecognizer;
 
+        private void Awake()
+        {
+            gestureRecognizer.OnSwipe += direction =>
+            {
+                switch (direction)
+                {
+                    case MiniGestureRecognizer.SwipeDirection.None:
+                    case MiniGestureRecognizer.SwipeDirection.Up:
+                        player.Move(0, 1);
+                        break;
+                    case MiniGestureRecognizer.SwipeDirection.Down:
+                        player.Move(0, -1);
+                        break;
+                    case MiniGestureRecognizer.SwipeDirection.Right:
+                        player.Move(1, 0);
+                        break;
+                    case MiniGestureRecognizer.SwipeDirection.Left:
+                        player.Move(-1, 0);
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException(nameof(direction), direction, null);
+                }
+            };
+        }
         private void Update()
         {
             if (player.IsMoving)
