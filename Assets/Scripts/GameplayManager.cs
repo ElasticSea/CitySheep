2c4cbfb947117b51765a2db7e2cf5d7b18b524f4
ElasticSea <info@elasticsea.com>
2018-03-13 15:03:53 +0100

    GameOver screen.

---
+++ Assets/Scripts/GameplayManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Assets.Core.Scripts.Camera;
+using DG.Tweening;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class GameplayManager : MonoBehaviour
+    {
+        [SerializeField] private Player player;
+
+        public event Action OnGameOver = () => { };
+
+        private void Awake()
+        {
+            Time.timeScale = 1;
+            player.OnKilled += () =>
+            {
+//                Camera.main.GetComponent<Follow>().SmoothFollow = false;
+                DOTween.To(() => Camera.main.GetComponent<Follow>().FollowSpeed, value => Camera.main.GetComponent<Follow>().FollowSpeed = value, 5, 0.3f).SetEase(Ease.InQuad).SetUpdate(true);
+                DOTween.To(() => Camera.main.GetComponent<Follow>().AfterOffset, value => Camera.main.GetComponent<Follow>().AfterOffset = value, Vector3.zero, 0.3f).SetEase(Ease.InQuad).SetUpdate(true);
+                DOTween.To(() => Time.timeScale, value => Time.timeScale = value, 0, 0.3f).SetEase(Ease.InQuad).SetDelay(.5f).OnComplete(() => OnGameOver()).SetUpdate(true);
+            };
+        }
+    }
+}
---
+++ Assets/Scripts/UiManager.cs
@@ -0,0 +1,27 @@
+﻿using Assets.Core.Scripts.Extensions;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+namespace Assets.Scripts
+{
+    public class UiManager : MonoBehaviour
+    {
+        [SerializeField] private GameObject gameOverScreen;
+        [SerializeField] private GameObject retryText;
+
+        [SerializeField] private GameplayManager gameplayManager;
+
+        private void Awake()
+        {
+            gameOverScreen.SetActive(false);
+
+            gameplayManager.OnGameOver += () =>
+            {
+                gameOverScreen.SetActive(true);
+            };
+
+            retryText.AddComponent<EventTrigger>().PointerClick(arg0 => SceneManager.LoadScene(SceneManager.GetActiveScene().name));
+        }
+    }
+}
