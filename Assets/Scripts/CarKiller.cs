6d973add8705851d2286ffaa78e6abd4be4a2ffe
ElasticSea <info@elasticsea.com>
2018-03-13 15:35:52 +0100

    Adding crash sound.

--- Assets/Scripts/CarKiller.cs
+++ Assets/Scripts/CarKiller.cs
@@ -5,12 +5,15 @@ namespace Assets.Scripts
     public class CarKiller : MonoBehaviour
     {
         [SerializeField] private Vector3 killVector;
+        [SerializeField] private AudioSource crashSound;
 
         private void OnTriggerEnter(Collider other)
         {
             var player = other.GetComponent<Player>();
             if (player)
             {
+                crashSound.Play();
+
                 var go = new GameObject("StickedTo");
                 go.transform.position = player.transform.position;
                 go.transform.SetParent(transform);
