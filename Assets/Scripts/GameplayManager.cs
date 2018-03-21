0086a0d378de4f098ad0a5ef21a74e61508612b4
ElasticSea <info@elasticsea.com>
2018-03-21 05:02:10 +0100

    Adding gameover animation .

--- Assets/Scripts/GameplayManager.cs
+++ Assets/Scripts/GameplayManager.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Assets.Core.Scripts.Camera;
+using Assets.Scripts.Ui;
 using DG.Tweening;
 using UnityEngine;
 
@@ -8,8 +9,7 @@ namespace Assets.Scripts
     public class GameplayManager : MonoBehaviour
     {
         [SerializeField] private Player player;
-
-        public event Action OnGameOver = () => { };
+        [SerializeField] private GameOverScreen gameOverScreen;
 
         private void Awake()
         {
@@ -24,7 +24,7 @@ namespace Assets.Scripts
                     .Insert(0, DOTween.To(() => follow.AfterOffset, value => follow.AfterOffset = value, Vector3.zero, 0.5f))
                     .Insert(0.5f, DOTween.To(() => Time.timeScale, value => Time.timeScale = value, 0, 0.3f))
                     .Insert(0.5f, DOTween.To(() => AudioListener.volume, value => AudioListener.volume = value, 0, 0.3f))
-                    .OnComplete(() => OnGameOver())
+                    .OnComplete(() => gameOverScreen.Show())
                     .SetUpdate(true);
             };
         }
---
+++ Assets/Scripts/Ui/GameOverScreen.cs
@@ -0,0 +1,39 @@
+﻿using Assets.Core.Scripts.Extensions;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+namespace Assets.Scripts.Ui
+{
+    public class GameOverScreen : MonoBehaviour
+    {
+        [SerializeField] private CanvasGroup gameOverScreen;
+        [SerializeField] private CanvasGroup retryText;
+
+        private Tween tween;
+
+        private void Awake()
+        {
+            gameOverScreen.gameObject.SetActive(false);
+            gameOverScreen.alpha = 0;
+            retryText.alpha = 0;
+
+            tween = DOTween.Sequence()
+                .Insert(0, gameOverScreen.DoFade(1, .4f))
+                .Insert(0.2f, retryText.DoFade(1, .4f))
+                .SetAutoKill(false)
+                .SetUpdate(true)
+                .Pause();
+
+            retryText.gameObject.AddComponent<EventTrigger>()
+                .PointerClick(arg0 => SceneManager.LoadScene(SceneManager.GetActiveScene().name));
+        }
+
+        public void Show()
+        {
+            gameOverScreen.gameObject.SetActive(true);
+            tween.PlayForward();
+        }
+    }
+}
