84b223dbd992f09c013bdab363c7b9c72648bb75
ElasticSea <info@elasticsea.com>
2018-03-23 11:28:42 +0100

    Fixing TMP issue on webgl .net 4.6

---
+++ Assets/Scripts/TMPWebglFix.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class TMPWebglFix : MonoBehaviour
+    {
+        private void Awake()
+        {
+            // Issue https://issuetracker.unity3d.com/issues/tmp-inputfield-throws-an-invalidoperationexception-on-webgl-with-net-4-dot-6
+            if (Application.platform == RuntimePlatform.WebGLPlayer)
+            {
+                DestroyImmediate(gameObject);
+            }
+        }
+    }
+}
