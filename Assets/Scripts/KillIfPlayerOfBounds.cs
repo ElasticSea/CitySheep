718adfc753cc707dd9827d12d44ca4e9374b1c57
ElasticSea <info@elasticsea.com>
2018-03-23 16:34:39 +0100

    Don't kill player immediaty when he leaves the screen, give him some leeway.

--- Assets/Scripts/KillIfPlayerOfBounds.cs
+++ Assets/Scripts/KillIfPlayerOfBounds.cs
@@ -5,11 +5,12 @@ namespace Assets.Scripts
     public class KillIfPlayerOfBounds : MonoBehaviour
     {
         [SerializeField] private Player player;
+        [SerializeField] private float margin = .1f;
 
         private void Update()
         {
             var screen = Camera.main.WorldToViewportPoint(player.transform.position);
-            if (screen.x < 0 || screen.x > 1 || screen.y < 0 || screen.y > 1)
+            if (screen.x < 0-margin || screen.x > 1+ margin || screen.y < 0 - margin || screen.y > 1 + margin)
             {
                 player.Kill();
             }
