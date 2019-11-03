581b883e6b09850d5bd0413cfefb9ac1206b6032
ElasticSea <info@elasticsea.com>
2019-11-03 16:12:42 +0100

    Using embeded resources.

---
+++ Assets/Scripts/Editor/EnableWebglEmbeddedResources.cs
@@ -0,0 +1,23 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Assets.Core.Scripts.Editor
+{
+    public class EnableWebglEmbeddedResources : MonoBehaviour
+    {
+        [MenuItem("WebGL/Enable Embedded Resources")]
+        public static void EnableWebGLErrorMessageTesting()
+        {
+#pragma warning disable CS0618 // Type or member is obsolete
+            PlayerSettings.SetPropertyBool("useEmbeddedResources", true, BuildTargetGroup.WebGL);
+#pragma warning restore CS0618 // Type or member is obsolete
+        }
+        [MenuItem("WebGL/Disnable Embedded Resources")]
+        public static void dddnableWebGLErrorMessageTesting()
+        {
+#pragma warning disable CS0618 // Type or member is obsolete
+            PlayerSettings.SetPropertyBool("useEmbeddedResources", false, BuildTargetGroup.WebGL);
+#pragma warning restore CS0618 // Type or member is obsolete
+        }
+    }
+}
